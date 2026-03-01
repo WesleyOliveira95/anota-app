eae4183848ab50f2ea56d7705a1b1490be6d07a5
Wesley de Oliveira <wesleydeoliveirapereira95@gmail.com>
2026-03-01 11:52:31 -0300

    colorsmodificado

--- lib/colors.ts
+++ lib/colors.ts
@@ -1,9 +1,9 @@
 export const colors = {
-  bg: '#b2adad',
-  card: '#b6afaf',
-  text: '#ffffff',
-  sub: '#686a75',
-  input: '#c70a0a',
-  border: '#8d8383',
-  accent: '#6a6262'
+  bg: '#dad2cd',//Claro1
+  card: '#cc772d',//claro2
+  text: '#000000',
+  sub: '#e8d8b9',//oscuro1
+  input: '#d35454',
+  border: '#c5b899',//medio1
+  accent: '#d5a47c'//medio2
 };
